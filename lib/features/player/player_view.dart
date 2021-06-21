eec75ee556ea9559abbfe155db281dbadb60bda1
Keagan Hilliard <keagan.hilliard@gmail.com>
2021-06-21 13:20:57 -0700

    Remove cast

--- lib/features/player/player_view.dart
+++ lib/features/player/player_view.dart
@@ -106,77 +106,77 @@ class PlayerView extends HookWidget {
                                 child: CircularProgressIndicator(),
                               ),
                             ),
-                            Positioned(
-                              top: 5.0,
-                              right: 5.0,
-                              child: Opacity(
-                                opacity: 1,
-                                child: IconButton(
-                                  color: Colors.grey[100],
-                                  icon: Icon(Icons.cast),
-                                  onPressed: () async {
-                                    showModalBottomSheet(
-                                        context: context,
-                                        builder: (context) {
-                                          return SingleChildScrollView(
-                                            child: Container(
-                                              child: Padding(
-                                                padding:
-                                                    const EdgeInsets.all(18.0),
-                                                child: FutureBuilder<
-                                                    List<CastDevice>>(
-                                                  future: CastDiscoveryService()
-                                                      .search(),
-                                                  builder: (context, snapshot) {
-                                                    if (snapshot.hasError) {
-                                                      return Center(
-                                                        child: Text(
-                                                          'Error: ${snapshot.error.toString()}',
-                                                        ),
-                                                      );
-                                                    } else if (!snapshot
-                                                        .hasData) {
-                                                      return Center(
-                                                        child:
-                                                            CircularProgressIndicator(),
-                                                      );
-                                                    }
+                            // Positioned(
+                            //   top: 5.0,
+                            //   right: 5.0,
+                            //   child: Opacity(
+                            //     opacity: 1,
+                            //     child: IconButton(
+                            //       color: Colors.grey[100],
+                            //       icon: Icon(Icons.cast),
+                            //       onPressed: () async {
+                            //         showModalBottomSheet(
+                            //             context: context,
+                            //             builder: (context) {
+                            //               return SingleChildScrollView(
+                            //                 child: Container(
+                            //                   child: Padding(
+                            //                     padding:
+                            //                         const EdgeInsets.all(18.0),
+                            //                     child: FutureBuilder<
+                            //                         List<CastDevice>>(
+                            //                       future: CastDiscoveryService()
+                            //                           .search(),
+                            //                       builder: (context, snapshot) {
+                            //                         if (snapshot.hasError) {
+                            //                           return Center(
+                            //                             child: Text(
+                            //                               'Error: ${snapshot.error.toString()}',
+                            //                             ),
+                            //                           );
+                            //                         } else if (!snapshot
+                            //                             .hasData) {
+                            //                           return Center(
+                            //                             child:
+                            //                                 CircularProgressIndicator(),
+                            //                           );
+                            //                         }
 
-                                                    if (snapshot
-                                                        .data!.isEmpty) {
-                                                      return Column(
-                                                        children: [
-                                                          Center(
-                                                            child: Text(
-                                                              'No Chromecast founded',
-                                                            ),
-                                                          ),
-                                                        ],
-                                                      );
-                                                    }
+                            //                         if (snapshot
+                            //                             .data!.isEmpty) {
+                            //                           return Column(
+                            //                             children: [
+                            //                               Center(
+                            //                                 child: Text(
+                            //                                   'No Chromecast founded',
+                            //                                 ),
+                            //                               ),
+                            //                             ],
+                            //                           );
+                            //                         }
 
-                                                    return Column(
-                                                      children: snapshot.data!
-                                                          .map((device) {
-                                                        return ListTile(
-                                                          title:
-                                                              Text(device.name),
-                                                          onTap: () {
-                                                            // _connectToYourApp(context, device);
-                                                          },
-                                                        );
-                                                      }).toList(),
-                                                    );
-                                                  },
-                                                ),
-                                              ),
-                                            ),
-                                          );
-                                        });
-                                  },
-                                ),
-                              ),
-                            ),
+                            //                         return Column(
+                            //                           children: snapshot.data!
+                            //                               .map((device) {
+                            //                             return ListTile(
+                            //                               title:
+                            //                                   Text(device.name),
+                            //                               onTap: () {
+                            //                                 // _connectToYourApp(context, device);
+                            //                               },
+                            //                             );
+                            //                           }).toList(),
+                            //                         );
+                            //                       },
+                            //                     ),
+                            //                   ),
+                            //                 ),
+                            //               );
+                            //             });
+                            //       },
+                            //     ),
+                            //   ),
+                            // ),
                             if (book!.displayDescription?.isNotEmpty ?? false)
                               Positioned(
                                 bottom: 5.0,
