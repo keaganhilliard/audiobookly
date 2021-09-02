fc6705475213d2e9e49a668e2fc8c2801e64247e
Keagan Hilliard <keagan.hilliard@gmail.com>
2021-09-02 10:00:43 -0700

    Fixed the android build

--- lib/features/player/player_view.dart
+++ lib/features/player/player_view.dart
@@ -11,7 +11,7 @@ import 'package:audiobookly/widgets/playback_position.dart';
 import 'package:audiobookly/widgets/rewind_button.dart';
 import 'package:audiobookly/widgets/seek_bar.dart';
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:cast/cast.dart';
+// import 'package:cast/cast.dart';
 import 'package:audiobookly/utils/jump_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
